e107eb4b1c27ee26211044dd90845b9a8cc9b9d2
Rijju Poudel <rezzusharma@gmail.com>
2024-12-11 16:30:35 +0545

    features:added step definations for login features
    Signed-off-by: Rijju Poudel <rezzusharma@gmail.com>

---
+++ tests/stepDefinitions/loginContext.js
@@ -0,0 +1,22 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given('the user has navigated to the login page', function () {
+    // Write code here that turns the phrase above into concrete actions
+    console.log('pending');
+  });
+
+
+  When('the user login with email {string} and password {string}', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';/
+    console.log('pending');
+  });
+
+
+  Then('the user should we navigated to the homepage.', function () {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';
+    console.log('pending');
+  });
+
+
