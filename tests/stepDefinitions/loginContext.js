bfd2a832a09faaf93b67b13c7990a36232d7211e
Rijju Poudel <rezzusharma@gmail.com>
2024-12-16 16:26:45 +0545

    implement step-definitions

--- tests/stepDefinitions/loginContext.js
+++ tests/stepDefinitions/loginContext.js
@@ -1,26 +1,38 @@
+const {expect} = require('@playwright/test');
 const { Given, When, Then } = require("@cucumber/cucumber");
 
-Given('the user {string} has navigated to the login page', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("solved1");
+const loginBtnLocator  = "(//input[@name='commit'])[1]"
+const emailFieldLocator = "#spree_user_email"
+const passwordFieldLocator = "#spree_user_password"
+const errorLocator = ".alert-danger"
+
+Given('the user {string} has navigated to the login page',  async function (user) {
+  await page.goto("http://localhost:3000/admin/login");
+  await expect(page).toHaveURL('http://localhost:3000/admin/login');
 });
 
-When('user {string} logs in with email {string} and password {string}', function (user, email, password) {
-  // Write code here that turns the phrase above into concrete actions
-  console.log(user, email, password)
+When('user {string} logs in with email {string} and password {string}', async function (user, email, password) {
+  await page.fill(emailFieldLocator, email);
+  await page.fill(passwordFieldLocator, password);
+  await page.click(loginBtnLocator);
 });
 
-Then('user {string} should be navigated to the admin panel dashboard', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("solved2")
+Then('user {string} should be navigated to the admin panel dashboard', async function (user) {
+  await page.waitForURL("http://localhost:3000/", { timeout: 5000 });
+});
+ 
+When('user {string} tries to log in with following details:', async function (user, dataTable) {
+ const credentials = dataTable.hashes();
+ for(const credential of credentials)
+ {
+  {
+    await page.locator(emailFieldLocator).fill(credential.email);
+    await page.locator(passwordFieldLocator).fill(credential.password);
+    await page.locator(loginBtnLocator).click();
+    }
+ }
 });
 
-When('user {string} tries to log in with following details:', function (string, dataTable) {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("solved")
-});
-
-Then('error message {string} should be shown', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  console.log("solved2")
+Then('error message {string} should be shown', async function (errorMessage) {
+  await expect(page.locator(errorLocator)).toHaveText(errorMessage);
 });
